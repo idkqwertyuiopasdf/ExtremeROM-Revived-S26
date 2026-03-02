50dba7b11eded7d7338f95331b9d93b51c88189a
idkqwertyuiopasdf <rehanhonortab@gmail.com>
2026-03-02 19:28:51 +0300

    add monsterrom stuff
    customize.sh

--- platform/exynos2100/patches/stock_blobs/customize.sh
+++ platform/exynos2100/patches/stock_blobs/customize.sh
@@ -1,10 +1,10 @@
-# S24 FE OneUI 7 -> SoundBooster 2000
+# S25 Ultra OneUI 8.5 -> SoundBooster 2060
 # S21 Series -> SoundBooster 1050
 # S21 FE -> SoundBooster 1070
 
 LOG_STEP_IN "- Replacing SoundBooster"
-DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2000.so"
-DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2000.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundBooster_ver2060.so"
+DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SAG_EQ_ver2060.so"
 DELETE_FROM_WORK_DIR "system" "system/lib64/libsoundboostereq_legacy.so"
 if [[ "$TARGET_CODENAME" != "r9s"  ]]; then
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/lib_SoundBooster_ver1050.so" 0 0 644 "u:object_r:system_lib_file:s0"
@@ -16,20 +16,16 @@ fi
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Replacing GameDriver"
+DELETE_FROM_WORK_DIR "system" "system/priv-app/GameDriver-SM8750"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/GameDriver-EX2100/GameDriver-EX2100.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/DevGPUDriver-EX2100/DevGPUDriver-EX2100.apk" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
 LOG_STEP_IN "- Replacing Hotword"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentOKGoogleEx4CORTEXM55"
-DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx4CORTEXM55"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentXGoogleEx6_WIDEBAND_LARGE"
+DELETE_FROM_WORK_DIR "product" "priv-app/HotwordEnrollmentYGoogleEx6_WIDEBAND_LARGE"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentOKGoogleEx3CORTEXM4/HotwordEnrollmentOKGoogleEx3CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "product" "priv-app/HotwordEnrollmentXGoogleEx3CORTEXM4/HotwordEnrollmentXGoogleEx3CORTEXM4.apk" 0 0 644 "u:object_r:system_file:s0"
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Adding 32-Bit WFD blobs from S21 (p3sxxx)"
-ADD_TO_WORK_DIR "p3sxxx" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
-ADD_TO_WORK_DIR "p3sxxx" "system" "system/lib" 0 0 644 "u:object_r:system_lib_file:s0"
 LOG_STEP_OUT
 
 if [[ "$TARGET_CODENAME" = "p3s" ]]; then
